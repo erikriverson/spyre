6c23f6a800a9559f6f618eac2cc6553c5ae145ef
Erik Iverson <erik@sigmafield.org>
2014-05-03 00:41:34 -0500

    implement toggle_connect

--- src/client/objector.js
+++ src/client/objector.js
@@ -1,7 +1,15 @@
 var iconController = function($scope) {
+    $scope.isConnected = false;
 
-    $scope.connect = function() {
-        $scope.ws.socket.close();
+    $scope.toggle_connect = function() {
+        if($scope.isConnected) {
+            $scope.ws.socket.close();
+            $scope.isConnected = false;
+        } else {
+            $scope.connect();
+            // need a valid check here to make sure this is true...
+            $scope.isConnected = true;
+        }
     };
 
 };
@@ -9,51 +17,45 @@ var iconController = function($scope) {
 var ObjectController = function($scope) {
     $scope.$watch('test', 
                   function() {
-                      console.log($scope.test);
                       $scope.objects = $scope.test;
                   });
 };
 
 
 var MainController = function($scope) {
-    try {
 
-        var w_socket = new FancyWebSocket("ws://127.0.0.1:7681");        
-        w_socket.bind('open', function() {
-            document.getElementById("statustd").style.color = "#40f300";
-        });
+    $scope.connect = function() {
+        try {
 
-        w_socket.bind('objects', function(msg) {
+            var w_socket = new FancyWebSocket("ws://127.0.0.1:7681");        
 
-            $scope.test = msg;
-            console.log($scope);
-        })
-            .bind('default', function(msg) {
-                show_default(msg.summary);
-                
-                var t_data = {
-                    "xScale": "ordinal",
-                    "yScale": "linear",
-                    "yMin": 0,
-                    "main": [
-                        {
-                            "data": msg.value
-                        }
-                    ]
-                };
-                t_data.main[0].className = "." + Math.random().toString(36).substring(7);
-                
-                var myChart = new xChart('bar', t_data, '#plot');
-            });
+            w_socket.bind('objects', function(msg) {
+                $scope.test = msg;
+            })
+                .bind('default', function(msg) {
+                    show_default(msg.summary);
+                    
+                    var t_data = {
+                        "xScale": "ordinal",
+                        "yScale": "linear",
+                        "yMin": 0,
+                        "main": [
+                            {
+                                "data": msg.value
+                            }
+                        ]
+                    };
+                    t_data.main[0].className = "." + 
+                        Math.random().toString(36).substring(7);
+                    
+                    var myChart = new xChart('bar', t_data, '#plot');
+                });
+            $scope.ws = w_socket; 
+        }
 
-        w_socket.bind('close',  function(){
-            document.getElementById("statustd").style.color = "#ff4040";
-        });
-        
-        $scope.ws = w_socket; 
-    }
+        catch(ex) {document.getElementById("output").textContent = "Error: " + ex;}
 
-    catch(ex) {document.getElementById("output").textContent = "Error: " + ex;}
+    };
 
     $scope.send_object = function(object_name) {
         w_socket.send("request_objects", object_name);
