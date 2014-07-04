2e0bb1673d0d5a4cfb03e255c8bab23431eec284
Erik Iverson <erik@sigmafield.org>
2014-07-04 18:19:05 -0500

    start building gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
 var gulp = require('gulp');  
-var clean = require('gulp-clean');
+var gutil = require('gulp-util');  
+var clean = require('gulp-clean');  
+var concat = require('gulp-concat');  
+var uglify = require('gulp-uglify');  
 
 gulp.task('clean', function () {  
-  return gulp.src('build', {read: false})
+  return gulp.src('src/client/build', {read: false})
     .pipe(clean());
+});
+
+gulp.task('vendor', function() {  
+  return gulp.src('src/client/vendor/*.js')
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('src/client/build'))
+    .pipe(filesize())
+    .pipe(uglify())
+    .pipe(rename('vendor.min.js'))
+    .pipe(gulp.dest('src/client/build'))
+    .pipe(filesize())
+    .on('error', gutil.log);
 });
 
 
