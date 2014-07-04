85cedca42a2911ccd6ace523e7bba6477fb50677
Erik Iverson <erik@sigmafield.org>
2014-07-04 17:41:44 -0500

    now using gulp

---
+++ src/spyre/tests/test.R
@@ -0,0 +1,9 @@
+library(devtools)
+
+document("/home/erik/Dropbox/src/projects/spyre/src/spyre")
+
+load_all("/home/erik/Dropbox/src/projects/spyre/src/spyre")
+library(ggplot2)
+data(mtcars)
+data(mpg)
+
