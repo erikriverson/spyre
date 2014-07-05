1a0d9043a3cc05bd2931349f30ed4a7fd9a0e53e
Erik Iverson <erik@sigmafield.org>
2014-07-05 00:14:32 -0500

    concat js files

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,32 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');  
 
 gulp.task('clean', function () {  
-  return gulp.src('src/client/build', {read: false})
-    .pipe(clean());
+    return gulp.src('src/client/build', {read: false})
+        .pipe(clean());
 });
 
-gulp.task('vendor', function() {  
-  return gulp.src('src/client/vendor/*.js')
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest('src/client/build'))
-    .pipe(filesize())
-    .pipe(uglify())
-    .pipe(rename('vendor.min.js'))
-    .pipe(gulp.dest('src/client/build'))
-    .pipe(filesize())
-    .on('error', gutil.log);
+gulp.task('js_vendor', function() {  
+    return gulp.src('src/client/vendor/*/*.js')
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('src/client/build'))
+        .on('error', gutil.log);
+});
+
+// process bower_components
+
+gulp.task('js_bower_components', function() {
+    return gulp.src('src/client/bower_components/**/*min.js')
+        .pipe(concat('bower.js'))
+        .pipe(gulp.dest('src/client/build'))
+        .on('error', gutil.log);
+});
+
+gulp.task('js_spyre', function() {
+    return gulp.src('src/client/scripts/**/*.js')
+        .pipe(concat('spyre.js'))
+        .pipe(gulp.dest('src/client/build'))
+        .on('error', gutil.log);
 });
 
 
+gulp.task('default', ['js_vendor', 'js_bower_components', 'js_spyre']);
