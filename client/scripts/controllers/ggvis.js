baf76c18290ca4ddc4cb56e8eba4b808bb2d9d28
Erik Iverson <erik@sigmafield.org>
2014-07-13 12:20:35 -0500

    stroke and fill colors for ggvis

--- client/scripts/controllers/ggvis.js
+++ client/scripts/controllers/ggvis.js
@@ -8,9 +8,10 @@ app.controller('mvController', function($scope, WSService) {
         });
     });
 
-    $scope.target = {x : "Not Set",
-                     y : "Not Set",
-                     fill : "#FFFFFF"
+    $scope.target = {xvar : "Not Set",
+                     yvar : "Not Set",
+                     fill : "#000000",
+                     stroke : "#000000"
                      };
 
     $scope.select = function(event, object) {
@@ -21,27 +22,39 @@ app.controller('mvController', function($scope, WSService) {
     
     $scope.$watchCollection('target', function(newValue, oldValue) {
         if(newValue !== oldValue) {
-            $scope.mv($scope.target.x, $scope.target.y, $scope.target.fill);
+            $scope.mv($scope.target);
         }
     });
 
-    $scope.mv = function(xvar_target, yvar_target, fill_target) {
-        console.log("Calling the mv function with" + xvar_target + "and" + yvar_target + "and" + typeof(fill_target));
+    $scope.mv = function(plot_spec) {
+        console.log(plot_spec);
+        var fill, stroke;
+        if(typeof(plot_spec.fill) !== "string") {
+            console.log('think fill is not a string');
+            fill = plot_spec.fill.data.object_index;
+        } else {
+            fill = plot_spec.fill;
+        }
 
-        if(typeof(fill_target) !== "string") {
-            fill_target = fill_target.data.object_index;
+        if(typeof(plot_spec.stroke) !== "string") {
+            stroke = plot_spec.stroke.data.object_index;
+        } else {
+            stroke = plot_spec.stroke;
         }
         
-        var mv_object = {xvar_target : xvar_target.data.object_index, 
-                         yvar_target: yvar_target.data.object_index,
-                         fill_target: fill_target};
+        var mv_object = {xvar : plot_spec.xvar.data.object_index, 
+                         yvar: plot_spec.yvar.data.object_index,
+                         fill: fill,
+                         stroke : stroke};
 
+        console.log("going to call mv with:");
         console.log(mv_object);
 
         WSService.send_r_data("ggvis_explorer", mv_object);
         return(0);
     };
 
-    $scope.scaledValue = 'false';
+    $scope.fill_scaled = 'false';
+    $scope.stroke_scaled = 'false';
     
 });
--- spyre/R/ggvis_explorer.R
+++ spyre/R/ggvis_explorer.R
@@ -7,17 +7,33 @@ ggvis_explorer <- function(D) {
 
 ggvis_spyre <- function(objs, names, ...) {
     message(paste0(names, collapse = " "))
+    message(paste0(str(objs), collapse = "\n"))
     summary <- paste(c(capture.output(cor(objs[[1]], objs[[2]]))))
 
     value <- as.data.frame(objs)
     value <- value[complete.cases(value), ]
     message(names(value))
 
-    ggvis_plot <- value %>% ggvis(~xvar_target, ~yvar_target, fill := ~fill_target) %>%
+    if(length(objs[["fill"]]) == 1) {
+        fill_scale <- objs[["fill"]]
+    } else {
+        fill_scale <- ~fill
+    }
+
+    if(length(objs[["stroke"]]) == 1) {
+        stroke_scale <- objs[["stroke"]]
+    } else {
+        stroke_scale <- ~stroke
+    }
+
+    ggvis_plot <- value %>%
+        ggvis(x = ~xvar, y = ~yvar, prop("fill", fill_scale),
+              prop("stroke", stroke_scale)) %>%
         layer_points()
             
     ggvis_spec <-
-        jsonlite::unbox(paste0(capture.output(show_spec(ggvis_plot)), collapse = ""))
+        jsonlite::unbox(paste0(capture.output(show_spec(ggvis_plot)),
+                               collapse = ""))
 
     list(event = "mv", data = list(summary = summary, value = ggvis_spec))
 }
