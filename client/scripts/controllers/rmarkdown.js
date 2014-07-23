7885953158c9cc240cad849e2f813485428ddc77
Erik Iverson <erik@sigmafield.org>
2014-07-23 18:52:10 -0500

    add rmarkdown support controller

---
+++ client/scripts/controllers/rmarkdown.js
@@ -0,0 +1,47 @@
+app.controller('rmdController', function($scope, WSService, $sce) {
+    $scope.$on('connected', function() {
+        WSService.register_ws_callback('rmd', function(msg) {
+            console.log("[rmd] message received");
+            console.log(msg.summary);
+            $scope.rmd_content = msg.summary[0];
+            $scope.$apply();
+            $scope.injectHTML();
+        });
+    });
+
+    $scope.injectHTML = function(){
+
+        var html_string = $scope.rmd_content;
+	var iframe = $('iframe#test_iframe').get(0);
+
+	var iframedoc = iframe.document;
+		if (iframe.contentDocument)
+			iframedoc = iframe.contentDocument;
+		else if (iframe.contentWindow)
+			iframedoc = iframe.contentWindow.document;
+
+	 if (iframedoc){
+		 iframedoc.open();
+		 iframedoc.writeln(html_string);
+		 iframedoc.close();
+	 } else {
+		alert('Cannot inject dynamic contents into iframe.');
+	 }
+
+    };
+
+
+    $scope.renderHtml = function(html_code)
+    {
+        console.log('[rmd] renderHTML called');
+        console.log(html_code);
+        return $sce.trustAsHtml(html_code);
+    };
+
+    $scope.rmd_file_path = "test.rmd";
+
+    $scope.submit_rmd_file_path = function() {
+        WSService.send_r_data("rmd_explorer", $scope.rmd_file_path);
+    };
+
+});
---
+++ spyre/R/rmd_explorer.R
@@ -0,0 +1,5 @@
+rmd_explorer <- function(doc) {
+    rmd_docpath <- rmarkdown::render(doc, quiet = TRUE)
+    rmd_content <- paste0(readLines(rmd_docpath), collapse = "\n")
+    jsonlite::toJSON(list(event = 'rmd' , data = list(summary = rmd_content, value = rmd_content)))
+}
